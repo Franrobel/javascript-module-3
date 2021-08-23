d497494c8ae8222807701f1b3bda3d17a9a2817b
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-08-23 13:05:58 +0200

    dudas para llamar episodios

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const renderEpisodesList = (episode) => {
+    episode.results.forEach( (i, index) => {
+        let listElem = document.createElement("li");
+        listElem.classList.add(`episode-${episode.results[index]}`);
+        listElem.style.listStyle = "none"
+        listElem.innerHTML =`<button>Episode ${episode.results[index++].i}</button>`
+        document.querySelector("#episodes-list").appendChild(listElem)
+console.log(i);
+        
+    });
+}
+
+const getRicMortyEpisodes = async () => {
+    const url = "https://rickandmortyapi.com/api/episode"
+    const fetchUrl = await fetch(url);
+    const toJS = await fetchUrl.json();
+    renderEpisodesList(toJS)
+    console.log(toJS)
+} 
+getRicMortyEpisodes()
