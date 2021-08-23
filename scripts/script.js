d53293c8a90a293fb89de84e03f6850b5d8ee13d
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-08-23 18:27:08 +0200

    500

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,23 @@
+const getEpisodeCard = (episode)=>{
+       const rightSide = document.querySelector("#right-side");
+       const divEpisode = document.createElement("div")
+       rightSide.appendChild(divEpisode)
+       divEpisode.innerHTML = `<h1>Episode${episode}</h1>`;
+       console.log(episode)
+}
+const getSingleEpisode = async (id) => {
+    try {   // try se usa para todo lo que hara la funcion si todo esta bien en los inputs 
+        const url = `https://rickandmortyapi.com/api/episode/${id}`
+        const response = await fetch(url)
+        const parsedRes = await response.json() // en parsedRes convertimos response (que es obj json) a un obt JS asi podemos aplicar metodos de JS
+       // clearContent()
+        getEpisodeCard(parsedRes)   
+    }
+   catch (e){
+   } 
+}
 const renderEpisodesList = (episodes) => {
+
     episodes.results.forEach( (elem, index) => {
         let listElem = document.createElement("li");
         listElem.classList.add(`episode-${index++}`);
@@ -6,9 +25,11 @@ const renderEpisodesList = (episodes) => {
         listElem.style.listStyle = "none"
         listElem.style.textAlign = "center"
         listElem.style.margin = "2px 5px 2px 5px"
-        listElem.innerHTML =`<button class="btn btn-success">Episode ${elem.id}</button>`
-        console.log(elem)
-        console.log(index)
+        listElem.innerHTML =`<a href="#" class="btn-link">Episode ${elem.id}</a>`;
+        listElem.onclick = () => {  
+            getSingleEpisode(episodes)
+             }
+             
 
     });
 }
