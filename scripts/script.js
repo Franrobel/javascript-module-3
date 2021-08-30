1502325c4b7f49051b8e1abe83090b94a27bc922
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-08-30 10:09:29 +0200

    prueba

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-const rightSide = document.querySelector("#right-side");
-const divEpisode = document.createElement("div")
-divEpisode.classList.add("date-season-episode", "container-fluid")
 
 
 /*const getCharacters =  async () => {
@@ -15,7 +12,7 @@ divEpisode.classList.add("date-season-episode", "container-fluid")
             console.log(e)
     } 
  }*/
-const getEpisodeCard = (episode) => {
+/*const getEpisodeCard = (episode) => {
     console.log(episode)
     rightSide.appendChild(divEpisode)
     divEpisode.innerHTML = `<h2>${episode.name}</h2> <h6>${episode.air_date} | ${episode.episode}</h6>`;
@@ -24,8 +21,7 @@ const getEpisodeCard = (episode) => {
     charactersEpisode.classList.add("characters-list", "row")
     divEpisode.appendChild(charactersEpisode)
 
-    const getCharacters = (character) => { 
-        console.log(character)
+    
     const cards = document.createElement("div");
     cards.classList.add("card", "character", "m-4");
     cards.style.width = "15rem";
@@ -38,27 +34,48 @@ const getEpisodeCard = (episode) => {
     const cardBody = document.createElement("div")
     cardBody.innerHTML = `<div class="card-body px-0"><h5 class="card-title">${elem.index} | elem.status"</h5> </div>`
     cards.appendChild(cardBody)
-  console.log(getCharacters(elem));
-    }
+    }*/
+    const rightSide = document.querySelector("#right-side");
+const divEpisode = document.createElement("div")
+divEpisode.classList.add("date-season-episode", "container-fluid")
+
+  const divContainCharac = document.createElement('div');
+  divContainCharac.classList.add("characters-conatiner", "row", "cols-4", "container-fluid")
+  const EachCharacter =  () => {
   }
+  const renderCharacactresCards = async (urlCharacters)=> {
+    const response = await fetch(urlCharacters);
+    console.log(urlCharacters)
+    const characObj = await response.json();
+    console.log(characObj)
+    rightSide.appendChild(divContainCharac)
+    const charactersEpisode = document.createElement("div");
+    charactersEpisode.classList.add("characters-list", "col")
+    divContainCharac.appendChild(charactersEpisode)
+    const cards = document.createElement("div");
+    cards.classList.add("card", "character", "m-4");
+    cards.style.width = "15rem";
+    charactersEpisode.appendChild(cards)
+    const characImage = document.createElement("img")
+    characImage.setAttribute('src', `${characObj.image}`);
+    characImage.setAttribute('alt', `${characObj.name}`); 
+    characImage.classList.add("card-img-top", "py-2");
+    cards.appendChild(characImage)
+    const cardBody = document.createElement("div")
+    cardBody.innerHTML = `<div class="card-body px-0"><h4 class="card-title">${characObj.name}</h4> <h5>${characObj.species} | ${characObj.status}</h5> </div>`
+    cards.appendChild(cardBody)
+    
+  }
+const getSingleEpisode = (episode) => {
+    rightSide.appendChild(divEpisode)
+    //const charactersEpisode = document.createElement("div");
+    divEpisode.innerHTML = `<h2>${episode.name}</h2> <h6>${episode.air_date} | ${episode.episode}</h6>`;
+    //charactersEpisode.classList.add("characters-list", "row")
+   // divEpisode.appendChild(charactersEpisode)
+    episode.characters.forEach( urlCharacters=>{
+        renderCharacactresCards(urlCharacters);
+    })
 
-
-const getSingleEpisode = async (id) => {
-    try {   // try se usa para todo lo que hara la funcion si todo esta bien en los inputs 
-        const url = `https://rickandmortyapi.com/api/episode/${id}`
-        const response = await fetch(url)
-        const parsedRes = await response.json() // en parsedRes convertimos response (que es obj json) a un obt JS asi podemos aplicar metodos de JS
-        const urlCharacters = `https://rickandmortyapi.com/api/character`
-        const responseOfCharac = await fetch(urlCharacters);
-        const parsedCharac = await responseOfCharac.json()
-        getCharacters(parsedCharac)
-
-        getEpisodeCard(parsedRes)   
-
-    }
-   catch (e){
-       console.log(e)
-   } 
 }
 
 const renderEpisodesList = (episodes, i) => {
@@ -73,7 +90,9 @@ const renderEpisodesList = (episodes, i) => {
         listElem.style.margin = "2px 5px 2px 5px"
         listElem.innerHTML =`<a href="#" class="btn-link">Episode ${elem.id}</a>`;
         listElem.onclick = () => {  
-            getSingleEpisode(elem.id)
+            getSingleEpisode(elem) //ESTABA HACIENDO EL ELEM.ID Y ERA SOLO ELEM (QUE ME DA EL OBJECTO)
+console.log(elem.id) // SOLO EL NUMERO DEL EPISODIO QUE ESTABA PIDIENDO
+console.log(elem)
 
              }
     });
@@ -111,6 +130,7 @@ const clearContent = () =>{
         element.parentElement.removeChild(element)
     })
 
+  document.querySelectorAll("characters-list").forEach((element) => element.parentElement.removeChild(element) )
     const button = document.querySelector("#more-episodes-btn")
     button.parentElement.removeChild(button)
 }
